89ffeff31283e64012044e779d39cebd2fca8581
andreihramogin <andrej.hramogin2@gmail.com>
2021-12-24 14:48:04 +0300

    Added 2 new parameters to SingleCellularAnimals.class and getters/setters for it.

--- src/SingleCellularAnimals.java
+++ src/SingleCellularAnimals.java
@@ -2,9 +2,32 @@ public class SingleCellularAnimals extends Animal {
 
     //TODO add two new parameters for SingleCellularAnimals
 
+    boolean isUnicellular;
+    boolean isBreathable;
+
      public SingleCellularAnimals(){
-       setMulticellular(false);
+       setIsMulticellular(false);
+       setIsConsumer(true);
+       setIspPhotosynthetic(false);
+
+       setIsUnicellular(true);
+       setIsBreathable(true);
      }
 
     //TODO Add setters and getters to properties
+    public boolean getIsUnicellular(){
+         return isUnicellular;
+    }
+    public void setIsUnicellular (boolean a){
+         isUnicellular = a;
+    }
+
+    public boolean getIsBreathable (){
+         return isBreathable;
+    }
+
+    public void setIsBreathable(boolean a){
+         isBreathable = a;
+    }
+
 }

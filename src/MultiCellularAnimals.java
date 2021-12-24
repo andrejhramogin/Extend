2a9e47f761319ec0e9af193bbfdeb293e93dae0b
andreihramogin <andrej.hramogin2@gmail.com>
2021-12-24 14:29:47 +0300

    Added 2 new parameters to MultiCellularAnimals.class and getters/settres for it.

--- src/MultiCellularAnimals.java
+++ src/MultiCellularAnimals.java
@@ -1,11 +1,31 @@
 public class MultiCellularAnimals extends Animal {
 
     //TODO add two new parameters for MultiCellularAnimals
+    private boolean isBreeding;
+    private boolean isUnicellular;
+
 
     public MultiCellularAnimals() {
-        setMulticellular(true);
+        setIsMulticellular(true);
+        setIsConsumer(true);
+        setIspPhotosynthetic(false);
     }
 
     //TODO Add setters and getters to properties
+
+    public boolean getIsBreeding() {
+        return isBreeding;
+    }
+    public void setIsBreeding(boolean a){
+        isBreeding = a;
+    }
+
+    public boolean getIsUnicellular(){
+        return isUnicellular;
+    }
+
+    public void setIsUnicellular(boolean a){
+        isUnicellular = a;
+    }
 
 }

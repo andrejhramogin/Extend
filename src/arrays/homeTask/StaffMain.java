3f8224a65fa3d07788e9591ee30901deff9d4f6b
andreihramogin <andrej.hramogin2@gmail.com>
2022-01-13 22:17:26 +0300

    1. Added properties name, age, salaryRate, grade. Properties are private, access throught setters and getters. 2. Initiated array Staff by real date.

--- src/arrays/homeTask/StaffMain.java
+++ src/arrays/homeTask/StaffMain.java
@@ -3,12 +3,20 @@ package arrays.homeTask;
 public class StaffMain {
 
     public static void main(String[] args) {
-        Staff[] staff; //TODO init this array by real date
-        Company companyA; //TODO init company
 
-        System.out.println("Company average age: " + companyA.averageStaffAge());
-        System.out.println("Company average salary: " + companyA.averageStaffSalary());
-        System.out.println("Company staff popular name: " + companyA.popularStaffName());
-        System.out.println("Company average name length " + companyA.averageNameLength());
+        Staff[] staff = new Staff[4]; //TODO init this array by real date
+        staff[0] = new Staff("Sidorovich Petr", 25, 990.5f, 10);
+        staff[1] = new Staff("Sidorov Andrej", 45, 1420.6f, 15);
+        staff[2] = new Staff("Ivanchuk Elena", 37, 1350.7f, 14);
+        staff[3] = new Staff("Zhuk Elena", 40, 1456.7f, 18);
+
+        System.out.println(staff[0].getName() + " " + staff[0].getAge()); // printing values of Staff objects
+
+      //  Company companyA = new Company(); //TODO init company
+
+//        System.out.println("Company average age: " + companyA.averageStaffAge());
+//        System.out.println("Company average salary: " + companyA.averageStaffSalary());
+//        System.out.println("Company staff popular name: " + companyA.popularStaffName());
+//        System.out.println("Company average name length " + companyA.averageNameLength());
     }
 }

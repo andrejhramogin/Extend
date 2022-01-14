2e17926e5aaa6ab4ccf30561b9917fae4fe03e40
andreihramogin <andrej.hramogin2@gmail.com>
2022-01-14 20:33:28 +0300

    New branch Home_task_3_answer

--- src/arrays/homeTask/Staff.java
+++ src/arrays/homeTask/Staff.java
@@ -1,6 +1,49 @@
 package arrays.homeTask;
 
 public class Staff {
+    //TODO add properties name, age, salaryRate, grade. Properties are private, access throught setters and getters.
+    private String name;
+    private int age;
+    private float salaryRate;
+    private int grade;
 
-   //TODO add properties name, age, salaryRate, grade. Properties are private, access throught setters and getters.
+    public Staff(String name, int age, float salaryRate, int grade) {
+        this.name = name;
+        this.age = age;
+        this.salaryRate = salaryRate;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public float getSalaryRate() {
+        return salaryRate;
+    }
+
+    public void setSalaryRate(float newSalaryRate){
+        salaryRate = newSalaryRate;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int newGrade){
+        grade = newGrade;
+    }
+
 }
